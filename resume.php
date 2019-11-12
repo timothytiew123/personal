12ea848ad47f7ce0bd10e32af7d9220cdd49dc1d
timothytiew123 <36878356+timothytiew123@users.noreply.github.com>
2019-11-12 20:18:40 +0200

    Add files via upload

---
+++ resume.php
@@ -0,0 +1,265 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Timothy Tiew</title>
+		<link rel="icon" href="images/guitar5.jpg">
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+		<link rel="stylesheet" type = "text/css" href = "stylesheet.css">
+		<link href='http://fonts.googleapis.com/css?family=Lato&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+<div class = 'container-fluid bg-light' id="maincontainer">
+	<!------------ HEADER SECTION ------------->
+	<div class = 'row' style="margin:0px">
+		<!------------ NAVBAR SECTION ------------->
+		<div class = 'col-md-8'>
+			<!-- A grey horizontal navbar that becomes vertical on small screens -->
+			<nav class="navbar navbar-expand-sm" style="margin:0px 0px 0px 50px;height:70px">
+
+  			<!-- Links -->
+ 				<ul class="navbar-nav">
+   					<li class="nav-item" id="navbarlinkstyle">
+      					<a class="nav-link" href="index.php">Main page</a>
+    				</li>
+    				<li class="nav-item" id="navbarlinkstyle">
+     					<a class="nav-link" href="resume.php">Resume</a>
+   					</li>
+    				<li class="nav-item" id="navbarlinkstyle">
+      					<a class="nav-link" href="contact.php">Contact</a>
+   					</li>
+   					<li class="nav-item" id="navbarlinkstyle">
+      					<a class="nav-link" href="blog.php">Blog</a>
+   					</li>
+ 				</ul>
+			</nav>
+		</div>
+		<!------------ SOCIAL MEDIA ICONS ------------->
+		<div class = 'col-md-4'>
+			<div class='row' style="margin-right: 20px;margin-top:10px; float:right">
+				<a href="https://www.facebook.com/timothy.tiew" target="_blank" class="fa fa-facebook fa-2x"></a>
+				<a href="https://twitter.com/Timothy_Tiew" target="_blank" class="fa fa-twitter fa-2x"></a>
+				<a href="https://www.instagram.com/timothytiew/" target="_blank" class="fa fa-instagram fa-2x"></a>
+				<a href="https://www.linkedin.com/in/timothy-tiew-709635159/" target="_blank" class="fa fa-linkedin fa-2x"></a>
+			</div>
+		</div>
+	</div>
+	<!------------ MAIN RESUME ------------->
+	<div class ='container-fluid' id='maincontainer' style="background-color:white">
+		<!------------ PERSONAL INFORMATION ------------->
+		<div class="row">
+			<div class="col-md-6">
+				<img src="images/photoofme.jpg" class= "mypicture1">
+			</div>
+			<div class="col-md-6">
+				<div class="row">
+					<div class="text-justify py-2" id="vertical-align" style="margin:10% 10% 10% 0%; font-size:25px;">	
+						<h2 style="margin-bottom:20px">GENERAL INFORMATION</h2>
+						<p>Name: Tiew Wei Chong Timothy</p>
+						<p>Age : 23</p>
+						<p>Email : timothytiew@gmail.com</p>
+						<p><a href="https://docs.google.com/document/d/1NV6xxGq5cT4PDOnhbX13uxOHeQpfR3nZWYEZhspR4Is/edit?usp=sharing"><img src='images/downloadresume.jpg' style="margin:0px; padding: 0px"></a>
+					</div>
+				</div>
+			</div>
+		</div>
+		<hr size="30" style="border-top: 1px solid lightgrey;">
+		<!------------ SKILLS SECTION ------------->
+		<!------------ EDUCATION ------------->
+		<div class='row'>
+			<div class="col-md-4">
+				<div class="text-right py-2">
+					<h2>Education</h2>
+				</div>
+			</div>
+		</div>
+		<div class='row'>
+			<div class="col-md-4">
+				<div class="text-right py-2">
+					<h6>August 2017-Current</h6>
+				</div>
+			</div>
+			<div class="col-md-2">
+			</div>
+			<div class="col-md-6">
+				<div class="text-justify py-2" style="padding-right:100px">
+					<p><b>National University of Singapore</b></p>
+					<p><i>Bachelor in Business Administration. Double major in Entrepreneurship and Finance</i></p>
+					<li>Current GPA: 4.58/5</li>
+				</div>
+			</div>
+		</div>
+		<br>
+		<div class='row'>
+			<div class="col-md-4">
+				<div class="text-right py-2">
+					<h6>January 2014 - January 2017</h6>
+				</div>
+			</div>
+			<div class="col-md-2">
+			</div>
+			<div class="col-md-6">
+				<div class="text-justify py-2" style="padding-right:100px">
+					<p><b>Victoria Junior College</b></p>
+					<li>Attained AAAA/A in GCE A Levels Examination</li>
+				</div>
+			</div>
+		</div>
+		<br><br>
+		<!------------ WORK EXPERIENCE ------------->
+		<div class='row'>
+			<div class="col-md-4">
+				<div class="text-right py-2">
+					<h2>Work Experience</h2>
+				</div>
+			</div>
+		</div>
+		<div class='row'>
+			<div class="col-md-4">
+				<div class="text-right py-2">
+					<h6>May 2019 - Current</h6>
+				</div>
+			</div>
+			<div class="col-md-2">
+			</div>
+			<div class="col-md-6">
+				<div class="text-justify py-2" style="padding-right:100px">
+					<p><b>REALly.sg</b></p>
+					<p><i>Business Consultant</i></p>
+					<li>Utilised both front-end and back-end web development skills in HTML, CSS, PHP and SQL to create the main marketing website of the company. I was responsible for displaying the relevant information from the database on to the main website in a aesthetically pleasing manner.</li><br>
+					<li>Developed and employed sales processes from the ground up. I was responsible for the creation of our hubspot contacts database, creating email templates and personal messages to reach out to onboard potential partners in the real estate industry for the sales team. </li>
+				</div>
+			</div>
+		</div>
+		<br>
+		<div class='row'>
+			<div class="col-md-4">
+				<div class="text-right py-2">
+					<h6>January 2017 - Current</h6>
+				</div>
+			</div>
+			<div class="col-md-2">
+			</div>
+			<div class="col-md-6">
+				<div class="text-justify py-2" style="padding-right:100px">
+					<p><b>Deliveroo Bicycle Rider</b></p>
+					<li>Part time job that I have beeen doing throughout my university in order to pay for my education and living expenses on my own. Was constantly placed in the top tier of riders on the metrics of fast delivery timings, punctuality for sessions and working peak periods.</li>
+					<br>
+				</div>
+			</div>
+		</div>
+		<div class='row'>
+			<div class="col-md-4">
+				<div class="text-right py-2">
+					<h6>March 2015 - January 2017</h6>
+				</div>
+			</div>
+			<div class="col-md-2">
+			</div>
+			<div class="col-md-6">
+				<div class="text-justify py-2" style="padding-right:100px">
+					<p><b>Naval Diver in the Singapore Navy</b></p>
+				</div>
+			</div>
+			
+		</div>
+
+
+		<!------------ Co-curricular activities ------------->
+		<div class='row'>
+			<div class="col-md-4">
+				<div class="text-right py-2">
+					<h2>Co-curricular clubs</h2>
+				</div>
+			</div>
+		</div>
+		<div class='row'>
+			<div class="col-md-4">
+				<div class="text-right py-2">
+					<h6>January 2018 - May 2019</h6>
+				</div>
+			</div>
+			<div class="col-md-2">
+			</div>
+			<div class="col-md-6">
+				<div class="text-justify py-2" style="padding-right:100px">
+					<p><b>AIESEC in NUS</b></p>
+					<p><i>Team Leader in Marketing Department</i></p>
+					<li>Was placed in charge of setting the culture and delegating tasks in a small team. Created and sent emails to the entire school population in order to inform and attract students to join AIESEC’s global Programmes. </li>
+					<br>
+					<li>Went on overseas conferences to Japan and Hong Kong and met and made friends with other university students all over the world. Exposed myself to people from different cultures and learnt how to adapt and make friends with people from all countries in an international environment.</li>
+				</div>
+			</div>
+		</div>
+		<!------------------- SKILLS AND HOBBIES ---------------------->
+		<div class='row'>
+			<div class="col-md-4">
+				<div class="text-right py-2">
+					<h2>Skills Proficiencies</h2>
+				</div>
+			</div>
+		</div>
+		<div class='row'>
+			<div class="col-md-4">
+				<div class="text-right py-2">
+					<h6>Professional Skills</h6>
+				</div>
+			</div>
+			<div class="col-md-2">
+			</div>
+			<div class="col-md-6">
+				<div class="text-justify py-2" style="padding-right:100px">
+					<p><b>Microsoft Office - Intermediate</b></p>
+					<li>Able to use the full suite of microsoft office, both offline and online to create and share material in a professional environment.</li>
+					<br>
+					<p><b>Web Development (HTML, CSS, PHP and SQL) - Intermediate</b></p>
+					<li>Able to create aesthetically pleasing static websites according to specifications. Able to create databases and link it to the front end for data creation, update and deletion. Able to create searchbar, blog and forum functionalities.</li>
+					<br>
+					<p><b>Prototyping Software (Axure and AppInventor2) - Intermediate</b></p>
+					<li>I can utilise prototyping software for both web apps and phone apps in order to visualise user journeys before embarking on coding the actual project.</li>
+					<br>
+					<p><b>Python Analytics - Intermediate </b></p>
+					<li>Able to utilise numpy and pandas package to modify and visualise dataframes. Able to conduct simulations, hypothesis testing and also regression on datasets. Able to draw insights from the data and present it in a report format with recommendations.</li>
+					<br>
+					<p><b>Sales skills (pitching, understanding clients needs) - Intermediate </b></p>
+					<li>Able to quickly understand customer's needs and concerns on the go and craft the appropriate response in order to increase the percentage chance of closing a sale. Able to deliver a pitch about my companies product's and highlighting the main value proposition of my company's offerings.</li>
+				</div>
+			</div>
+		</div>
+		<br>
+		<div class='row'>
+			<div class="col-md-4">
+				<div class="text-right py-2">
+					<h6>Interests</h6>
+				</div>
+			</div>
+			<div class="col-md-2">
+			</div>
+			<div class="col-md-6">
+				<div class="text-justify py-2" style="padding-right:100px">
+					<p><b>Travelling, web-development, cycling, playing pool, going to the gym, learning a new language, meeting new people, stand-up comedy, epic movies, arm-wrestling.</b></p>
+				</div>
+			</div>
+		</div>
+	</div>
+	
+
+	<!------------------- FOOTER SECTION -------------------------->
+	<div class = 'container-fluid' id="maincontainer" style="background-color: #e0f7fa">
+		<div class ="text-center py-2" style="color: #696969"><b>"Life is what happens when you are busy making other plans"</b></div>
+			<div class = 'row'>
+				<div class='text-center' style="margin-left:auto; margin-right:auto;">
+					<a href="https://www.facebook.com/timothy.tiew" target="_blank" class="fa fa-facebook fa-2x"></a>
+					<a href="https://twitter.com/Timothy_Tiew" target="_blank" class="fa fa-twitter fa-2x"></a>
+					<a href="https://www.instagram.com/timothytiew/" target="_blank" class="fa fa-instagram fa-2x"></a>
+					<a href="https://www.linkedin.com/in/timothy-tiew-709635159/" target="_blank" class="fa fa-linkedin fa-2x"></a>
+				</div>
+			</div>
+		<div class="text-center py-2" style="color: #696969"><b>© 2019 Copyright:
+    	<a>Timothy Tiew</a>&nbsp&nbsp&nbsp&nbsp&nbsp<a>Best viewed in IE/Chrome Web browsers</a>
+  		</b></div>
+	</div>
+</div>
+
+</body>
+</html>
